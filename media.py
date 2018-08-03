2dfaa06a18ee26e89f006b26f6d89e8de0c8375c
JEEVESH KATARIA <jeeveshkataria92@gmail.com>
2018-08-03 21:37:31 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,11 +1,14 @@
 import webbrowser
+" webbrowser is used to play the trailer of movie in browser"
 
 class Movie():
 	def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube):
+		#"constructor is used to initialize the value of an object"
 		self.title=movie_title
 		self.storyline=movie_storyline
 		self.poster_image_url=poster_image
 		self.trailer_youtube_url=trailer_youtube
 
-	def show_trailer(self):
+	def show_trailer(self): 
+		#method is used to open the trailer of a movie
 		webbrowser.open(self.trailer_youtube_url)
